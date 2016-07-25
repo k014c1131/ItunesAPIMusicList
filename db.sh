6a161d8f63ceb66a4249332bdbbba704a6424250
k014c1141 <k014c1141@it-neec.jp>
2016-07-25 13:24:37 +0900

    db.shとdbmusicをくっつけたファイルです

--- db.sh
+++ db.sh
@@ -1,10 +1,15 @@
 #コメントアウトは #を使う。
 #最初のファイルです.
 #!/bin/bash
-mysql -uroot  -e "drop database ListDB;"
-mysql -uroot  -e "create database ListDB character set UTF8;"
-mysql -uroot  ListDB -e "create table list (id MEDIUMINT NOT NULL AUTO_INCREMENT  ,
-ListName varchar(256) ,createDate varchar(256), PRIMARY KEY(id));"
 
 #ListDB -いわゆる、マイリスト
+mysql -uroot  -e "drop database ListDB;"
+mysql -uroot  -e "create database ListDB character set UTF8;"
+mysql -uroot  ListDB -e "create table list(id MEDIUMINT NOT NULL AUTO_INCREMENT  ,
+ListName varchar(256) ,createDate varchar(256), PRIMARY KEY(id));"
+
 #ListDBに入っている曲名データ用のデータベース
+#mysql -uroot -e "create database MusicListDB character set UTF8;"
+mysql -uroot ListDB -e "create table musiclist(id MEDIUMINT NOT NULL AUTO_INCREMENT ,
+SongName varchar(256),Artist varchar(256),album varchar(256), ReleaseDate varchar(256),ListDBID int(0),
+previewUrl varchar(256),imageUrl varchar(256));"
