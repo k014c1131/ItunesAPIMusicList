7c2e7b898aba5b87478841a26f21ff110b1f6cad
sano <k014c1139@it-neec.jp>
2016-09-12 13:23:17 +0900

    list

--- list.php
+++ list.php
@@ -29,15 +29,15 @@ th {
 <div   style="text-align:center;">
 
   <div   style="float:left;">
-      <input type="button" value="Back" onclick="history.back()" style="text-align:left;">
-                                        //音楽検索の画面から戻るボタンで戻る
+      <input type="button" value="Back" onclick="location.href='MusicList.php'" style="text-align:left;">
+
     </div>
       List
       <RIGHT>
         <div style="float:right;">
       <INPUT type="text" name="text">
       <input type="button" value="Create" onclick="history.back()">
-                                          //新しいリストを作成する
+
     </div>
     </RIGHT>
 </div>
@@ -48,12 +48,33 @@ th {
 	<tbody>
 		<tr>
 			<td>   <div Align="right"><input type="button" value="Delete" onclick="history.back()"></div></td>
-                                                                    //リストの削除
+
+      <?php
+        $dsn ='mysql:dbname=ListDB;host=localhost;charset=utf8';//項目の表示
+        $user='root';
+        $password ='';
+        try {
+          $dsn= new PDO($dsn,$user,$password,array(PDO::ATTR_ERRMODE => false));
+
+          $sql = 'SELECT * FROM list';
+          $stmt = $dsn->prepare($sql);
+          $stmt->execute();
+          while($task = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            echo'<label>'.$task['ListName'].' '.$task['createDate'].'</label>';
+            echo'<form method="get" class="log" action="list.php" >';
+          }
+
+        } catch (Exception $ex) {
+          print('リストの追加に失敗しました<br>');
+        }
+        $dsn=NULL;
+         ?>
 		</tr>
 
 	</tbody>
-</table>
-</div>
-    </body>
+
+      </table>
+      </div>
+  </body>
 
 </html>
